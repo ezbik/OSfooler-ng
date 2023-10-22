30f1eff4079d046584596032b64cb025ba99c01d
Pavel Piatruk <erik.zbik@gmail.com>
2023-10-22 21:13:53 +0300

    m

--- osfooler_ng/osfooler_ng.py
+++ osfooler_ng/osfooler_ng.py
@@ -935,8 +935,12 @@ def main():
   # Main program begins here
   show_banner()
   parser = optparse.OptionParser()
-  parser.add_option('-M', '--marked', action='store',
-                    dest='marked', help="process only packets with FWMARK, can be 1000 (dec) or (hex) 0x3e8 or value/mask")
+  parser.add_option('-M', '--marked', action='store', dest='marked', 
+                    help="process only packets with FWMARK, can be 1000 (dec) or (hex) 0x3e8 or value/mask")
+  parser.add_option('--cgroup_classid', action='store', dest='cgroup_classid', 
+                    help="process only packets coming from this Cgroup classid. Example: 1000 (dec) or 0x3e8 (hex)")
+  parser.add_option('--cgroup_path', action='store', dest='cgroup_path', 
+                    help="process only packets coming from this Cgroup path. Example: proxy.slice ")
   parser.add_option('-q', '--qnum', action='store',
                     dest='qnum', help="NFQUEUE id to use")
   parser.add_option('-n', '--nmap', action='store_true',
@@ -1062,6 +1066,12 @@ def main():
   if opts.marked:
     print (" [+] will process only packets marked as %s" % opts.marked)
     I_MARK="-m mark --mark  %s" % opts.marked
+  else if opts.cgroup_path:
+    print (" [+] will process only packets from Cgroup Path  %s" % opts.cgroup_path)
+    I_MARK="-m cgroup --path %s" % opts.cgroup_path
+  else if opts.cgroup_classid:
+    print (" [+] will process only packets from Cgroup classid  %s" % opts.cgroup_classid)
+    I_MARK="-m cgroup --classid %s" % opts.cgroup_classid
   else:
     I_MARK=""
 
