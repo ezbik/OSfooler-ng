ceb1504797f14b7f083a8093159426b142574e29
Pavel Piatruk <erik.zbik@gmail.com>
2023-10-23 01:45:49 +0300

    m

--- osfooler_ng/osfooler_ng.py
+++ osfooler_ng/osfooler_ng.py
@@ -1126,6 +1126,7 @@ def add_iptables_rules_p0f(iptables_conditions, q_num1 ):
         ret=os.system( iptables_line )
         if ret != 0:
             print " [+] could not add Iptables rule"
+            del_iptables_rules_p0f(iptables_conditions, q_num1 )
             sys.exit(' [+] Aborting...')
         
 def del_iptables_rules_p0f(iptables_conditions, q_num1 ):
